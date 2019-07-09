1309da3bbb851b8cb8e7a8196f1ac61e50398818
Mary West <westmarye1219@gmail.com>
2019-07-08 19:57:29 -0500

    added card

---
+++ src/components/ScatCard/ScatCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './ScatCard.scss';
+
+class ScatCard extends React.Component {
+  render() {
+    const { scat } = this.props;
+    return (
+      <div className="ScatCard">
+        <div className="card">
+  <div className="card-body">
+    <h5 className="card-title">{scat.sampleName}</h5>
+    <p className="card-text">{scat.location}</p>
+    {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+  </div>
+  </div>
+      </div>
+    );
+  }
+}
+
+export default ScatCard;
