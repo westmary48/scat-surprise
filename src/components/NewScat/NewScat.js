6698819edd015202782fc9a88a2d8988c9b6019f
Mary West <westmarye1219@gmail.com>
2019-07-09 18:37:40 -0500

    can see changes with sample name in the state in react console

--- src/components/NewScat/NewScat.js
+++ src/components/NewScat/NewScat.js
@@ -2,15 +2,43 @@ import React from 'react';
 
 import './NewScat.scss';
 
+const defaultScat = {
+  location: '',
+  weight: '',
+  color: '',
+  sampleName: '',
+  animal: '',
+
+};
+
 class NewScat extends React.Component {
+  state = {
+    newScat: defaultScat,
+
+  }
+
+  sampleNameChange = (e) => {
+    const tempScat = { ...this.state.newScat };
+    tempScat['sampleName'] = e.target.value;
+    this.setState({ newScat: tempScat });
+  }
+
   render() {
+    const { newScat } = this.state;
     return (
       <div className="NewSct">
         <h1>New Scat</h1>
         <form>
           <div className="form-group">
           <label htmlFor="sampleName">Sample Name</label>
-            <input type="email" className="form-control" id="sampleName" placeholder="Sample Name"/>
+            <input
+            type="email"
+            className="form-control"
+            id="sampleName"
+            placeholder="Sample Name"
+            value= {this.sampleName}
+            onChange = {this.sampleNameChange}
+            />
             </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
